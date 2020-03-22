0b9ba54d3335a8c755503104f3062b899d759a58
Lianghe77 <810949252@qq.com>
2020-03-22 11:32:24 +0800

    Add files via upload

--- onehot.py
+++ onehot.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-
+#该onehot有问题
 def onehot(data, n):
-    buf = np.zeros(data.shape + (n, ))
-    nmsk = np.arange(data.size)*n + data.ravel()
+    buf = np.zeros(data.shape + (n, ))    #(160,160,n)
+    nmsk = np.arange(data.size)*n + data.ravel()      #ravel()将data扁平化
     buf.ravel()[nmsk-1] = 1
     return buf
 
     
-
+#该写的onehot
+def onehot(data,n):
+    buf = np.zeros(data.shape + (n, ))
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            buf[i][j][abs(data[i][j]-1)] = 1
+    return buf
